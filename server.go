8422c2547a1126ee4756df7f013752ab33e18ebb
Foster Hoff <Fos@Fosters-MBP.attlocal.net>
2015-07-22 00:13:03 -0700

    replaced c3 with proper d3

---
+++ server.go
@@ -0,0 +1,157 @@
+package main
+import(
+    "net/http"
+    "text/template"
+    "goquery"
+    "log"
+    "fmt"
+    "strings"
+    "strconv"
+    "regexp"
+    "bytes"
+    "encoding/json"
+    "io/ioutil"
+    "os"
+    "bufio"
+)
+
+type Context struct {
+    Terms string
+    Category string
+    Condition string
+}
+
+type data_struct struct {
+    Prices []float64
+    Times []string
+}
+
+
+func myPostFunc(w http.ResponseWriter, req *http.Request){
+    decoder := json.NewDecoder(req.Body)
+    var t Context
+    err := decoder.Decode(&t)
+    if err != nil {
+        panic(err)
+    }
+    prices, times := getPrices(t.Terms, t.Category, t.Condition)
+    ds := data_struct{prices, times}
+    b,err := json.Marshal(ds)
+    if err != nil{
+        log.Fatal(err)
+    }else{
+        w.Write(b)
+    }
+}
+func myHandlerFunc(w http.ResponseWriter, req *http.Request){
+    bytedoc, err := ioutil.ReadFile("doc.html")
+    if err == nil{
+        doc := string(bytedoc)
+        w.Header().Add("Content Type", "text/html")
+        tmpl, err := template.New("anyNameForTemplate").Parse(doc)
+        if err == nil{
+            context := Context{
+                "Todd",
+                "hey",
+                "eBay sold prices",
+            }
+            tmpl.Execute(w, context)
+        }
+    }else{
+        log.Fatal(err)
+    }
+}
+
+func priceFinder(firstPrice string) (float64){
+    reg, err := regexp.Compile("[^0-9.]")
+    if err != nil{
+        log.Fatal(err)
+    }
+    if (strings.Count(firstPrice, "$") > 1){
+        price, err := strconv.ParseFloat(reg.ReplaceAllString(strings.Split(firstPrice, "$")[1], ""), 64)
+        if err != nil{
+            log.Fatal(err)
+        }else{
+            return price
+        }
+    }else{
+        price, err := strconv.ParseFloat(reg.ReplaceAllString(firstPrice, ""), 64)
+        if err != nil{
+            log.Fatal(err)
+        }else{
+            return price
+        }
+    }
+    return 0.0
+}
+
+
+func getPrices(item string, category string, condition string) ([]float64, []string){
+    var buffer bytes.Buffer
+    buffer.WriteString(fmt.Sprint("http://www.ebay.com/sch/",category,"/i.html?_from=R40&LH_Complete=1&LH_Sold=1&_nkw=",item,condition))
+    doc, err := goquery.NewDocument(buffer.String())
+    if err != nil {
+            log.Fatal(err)
+    }
+    var prices []float64
+    var times []string
+    doc.Find("li.sresult.lvresult.clearfix").Each(func(i int, s *goquery.Selection) {
+
+            t := s.Find("span.bold.bidsold").Text()
+            firstPrice := strings.Split(t, "Trending at")[0]
+
+            price := priceFinder(firstPrice)
+            time  := strings.Split(s.Find("span.tme").Text(), " ")
+
+            prices = append(prices, price)
+            times = append(times, strings.TrimSpace(time[0]))
+    })
+    return prices, times
+}
+
+
+type MyHandler struct{
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    path := r.URL.Path[1:]
+    log.Println(path)
+    f, err := os.Open(path)
+
+
+    if err == nil {
+        bufferedReader := bufio.NewReader(f)
+
+        var contentType string
+
+        if strings.HasSuffix(path, ".css"){
+            contentType = "text/css"
+        }else if strings.HasSuffix(path, ".html"){
+            contentType = "text/html"
+        }else if strings.HasSuffix(path, ".js"){
+            contentType = "application/javascript"
+        }else if strings.HasSuffix(path, ".png"){
+            contentType = "image/png"
+        }else if strings.HasSuffix(path, ".svg"){
+            contentType = "image/svg+xml"
+        }else if strings.HasSuffix(path, ".mp4"){
+            contentType = "video/mp4"
+        }else{
+            contentType = "text/plain"
+        }
+
+        w.Header().Add("Content Type", contentType)
+        bufferedReader.WriteTo(w)
+    } else {
+        w.WriteHeader(404)
+        w.Write([]byte("404 My Friend - " + http.StatusText(404)))
+    }
+}
+
+func main(){
+    http.HandleFunc("/postit", myPostFunc)
+    http.Handle("/index.html", new(MyHandler))
+    http.Handle("/atad.csv", new(MyHandler))
+    http.HandleFunc("/", myHandlerFunc)
+    http.ListenAndServe(":8080", nil)
+}
